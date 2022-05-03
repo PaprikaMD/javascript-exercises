bec7192e04bd24254954bcca984d83bf43bbb798
Grant Deedler <grantdeedler@gmail.com>
2022-05-03 17:50:44 -0400

    Complete fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    var reducedArray = [];
+    for(const item of array){
+        let matches = false;
+        for(const element of elements){
+            if(item === element){
+                matches = true;
+            }
+        }
 
+        if(!matches){
+            reducedArray.push(item);
+        }
+    }
+    return reducedArray;
 };
 
 // Do not edit below this line
