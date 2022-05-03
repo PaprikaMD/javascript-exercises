7299e45eeb5a3b1e3404a8ace6d810dcef4abc90
Grant Deedler <grantdeedler@gmail.com>
2022-05-03 17:40:21 -0400

    Complete third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var array = string.split('');
+    var reversedString = '';
+    for(let i = array.length-1; i >= 0; i--){
+        reversedString += array[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
