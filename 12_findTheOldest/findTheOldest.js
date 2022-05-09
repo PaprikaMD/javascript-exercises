d631d1f0a08984384f72658e62f09cb5f963fc1e
Grant Deedler <grantdeedler@gmail.com>
2022-05-09 11:38:02 -0400

    Complete 12th problem

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((lastPerson, currPerson) => {
+        if(calculateAge(lastPerson) < calculateAge(currPerson)) return currPerson;
+        return lastPerson;
+    })
 
 };
+
+function calculateAge(person) { 
+    if(person.yearOfDeath === undefined) person.yearOfDeath = new Date().getFullYear();
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
