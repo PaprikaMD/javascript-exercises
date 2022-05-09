4cd55edf4555419ebe7f31dd5b889843a55dab49
Grant Deedler <grantdeedler@gmail.com>
2022-05-09 11:06:47 -0400

    Complete 10th problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 1){
+        return 'OOPS';
+    }
 
+    let numberPair = [1,1];
+    for(let i = 0; i < num-2; i++){
+        numberPair.push(numberPair[0] + numberPair[1]);
+        numberPair.shift();
+    }
+    return numberPair[1];
 };
 
 // Do not edit below this line
