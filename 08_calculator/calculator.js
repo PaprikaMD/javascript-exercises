3c41f2c0c7f48280bec9dca2eae05e62c6e9302e
Grant Deedler <grantdeedler@gmail.com>
2022-05-09 10:11:13 -0400

    Complete 8th assignment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  var product = 1;
+  for(let i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x === 0) return 1;
+  let factorial = 1;
+  for(let i = x; i > 0; i--){
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
