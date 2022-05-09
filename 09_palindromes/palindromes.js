325918a2cc1d53b726d76d7d5cbc173947986c9b
Grant Deedler <grantdeedler@gmail.com>
2022-05-09 10:49:43 -0400

    Complete 9th assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    string = string.split('').filter(char => /[a-z]/.test(char));
+    for(let i = 0; i < string.length; i++) {
+        if(string[i] !== string[string.length - (i + 1)]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
